b8dc4e87f2c004280bad1fe2ae5c2b0a29c2caac
Anna Karingal <anna.karingal@gmail.com>
2015-04-08 16:36:58 -0400

    Now reading input buttons successfully

--- theremin.ino
+++ theremin.ino
@@ -67,6 +67,23 @@ void loop() {
     midiNoteOff(0, i, 127);
   }
   
+  
+  uint8_t buttons = lcd.readButtons();
+  
+  
+  if (buttons) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if (buttons & BUTTON_UP) {
+      lcd.print("UP ");
+      lcd.setBacklight(ON);
+    }
+    if (buttons & BUTTON_DOWN) {
+      lcd.print("DOWN ");
+      lcd.setBacklight(OFF);
+    }
+  }
+  
   delay( 1000 );
 }
 
